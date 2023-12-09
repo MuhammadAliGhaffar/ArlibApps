654f12d23b716eab274f6f5bf6ce2ee2050b7f7c
MuhammadAliGhaffar <alibrohi999@gmail.com>
2023-12-09 20:42:46 +0500

    theming

--- app/src/main/java/com/example/arlibapps/MainActivity.kt
+++ app/src/main/java/com/example/arlibapps/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.arlibapps
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arlibapps.navigation.NavigationHost
+import com.example.arlibapps.ui.theme.ArlibAppsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +25,13 @@ class MainActivity : ComponentActivity() {
     @Preview(showBackground = true, device = "id:pixel_5", showSystemUi = true)
     @Composable
     fun AppView() {
-        NavigationHost()
+        ArlibAppsTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                NavigationHost()
+            }
+        }
     }
 }
