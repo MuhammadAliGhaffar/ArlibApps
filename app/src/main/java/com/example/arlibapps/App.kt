566a1a489fb2d2147046a4a56d117f3255bb36df
MuhammadAliGhaffar <alibrohi999@gmail.com>
2023-12-09 13:50:40 +0500

    hilt added

---
+++ app/src/main/java/com/example/arlibapps/App.kt
@@ -0,0 +1,12 @@
+package com.example.arlibapps
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
