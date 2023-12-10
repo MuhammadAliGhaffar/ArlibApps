da6595c796604d0dab13d8a5c772e99b295d8e96
MuhammadAliGhaffar <alibrohi999@gmail.com>
2023-12-10 13:22:54 +0500

    top header added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,4 +98,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    //Material icons
+    implementation("androidx.compose.material:material-icons-extended:1.2.0")
 }
